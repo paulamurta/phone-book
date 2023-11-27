9d9229abdcc39aab9ca49040be4ab422bc636d9f
paulamurta <paulamurta@hotmail.com>
2023-11-27 19:32:20 -0400

    fix: fix DefaulTheme export

--- phone-book/frontend/src/styles/themes/styled.d.ts
+++ phone-book/frontend/src/styles/themes/styled.d.ts
@@ -3,5 +3,5 @@ import { theme } from "./theme";
 declare module "styled-components" {
   type CustomTheme = typeof theme;
 
-  export type DefaultTheme = CustomTheme;
+  export interface DefaultTheme extends CustomTheme {}
 }
