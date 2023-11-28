de417dfd807e99f1f0ff0cd14f3876ffcd4470bb
paulamurta <paulamurta@hotmail.com>
2023-11-28 09:40:36 -0400

    fix: resolve CORS issue

--- phone-book/backend/src/app.ts
+++ phone-book/backend/src/app.ts
@@ -5,13 +5,8 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 
 app.listen(3009, () => {
   console.log("Running at port 3009");
