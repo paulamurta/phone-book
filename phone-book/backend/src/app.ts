a86119de8dafadb3a1594dc0e27965265047b89a
paulamurta <paulamurta@hotmail.com>
2023-11-28 03:31:09 -0400

    feat: add cors policy

--- phone-book/backend/src/app.ts
+++ phone-book/backend/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import router from "./routes/contact.routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.listen(3009, () => {
   console.log("Running at port 3009");
